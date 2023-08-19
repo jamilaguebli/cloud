3965ed86f608dec2d75846a12c356ac43e400094
jamilaguebli <jamilaguebli02@gmail.com>
2023-08-19 07:11:50 -0400

    adddd

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12935.543191391735!2d10.600519197440482!3d35.85180375282209!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd8a717817f21d%3A0xecbe7d2d204a3082!2z2K7Ystin2YXYqSDYp9mE2LTYsdmC2YrYqSwgU291c3Nl!5e0!3m2!1sfr!2stn!4v1692442597865!5m2!1sfr!2stn" style={{ width:"600" ,height:"450", style:"border:0", allowfullscreen:"" ,loading:"lazy", referrerpolicy:"no-referrer-when-downgrade"}}></iframe>
     </div>
   );
 }
